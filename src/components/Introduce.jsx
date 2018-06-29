0f9267d10e3692a2855b19414043edd5fffb1a6a
lufei <905729088@qq.com>
2018-06-29 10:17:57 +0800

    6.29Introduce页样式完成

--- src/components/Introduce.jsx
+++ src/components/Introduce.jsx
@@ -6,11 +6,11 @@ export default class Introduce extends React.Component {
     render(){
         const styles = Introduce.styles
         return (<HLayout style={styles.main}>
-            <div>
-                <div>为开发人员打造</div> 
-                <div>GitHub是一个受你工作方式启发的开发平台。从开源到商业，您可以托管和查看代码，管理项目，并与数百万其他开发人员一起构建软件。</div>
+            <div style={styles.IntroduceLeft}>
+                <div  style={styles.IntroduceLeftHeader}>为开发人员打造</div> 
+                <div  style={styles.IntroduceLeftMain}>GitHub是一个受你工作方式启发的开发平台。从开源到商业，您可以托管和查看代码，管理项目，并与数百万其他开发人员一起构建软件。</div>
             </div>
-            <div><Login /></div>
+            <div  style={styles.IntroduceRight}><Login /></div>
         </HLayout>)
     }
 }
@@ -18,5 +18,20 @@ export default class Introduce extends React.Component {
 Introduce.styles = {
     main: {
         justifyContent: 'center'
-    }
+    },
+    IntroduceLeft: {
+        margin:'243px 70px 0 0',
+        width:' 510px',
+        height: '464px',
+    },
+    IntroduceLeftHeader: {
+       padding:'25px 0 30px 0',
+       fontSize:'48px'
+    },
+    IntroduceLeftMain: {
+        fontSize:'28px'
+    },
+    IntroduceRight: {
+        width:' 507px',
+    },
 }
