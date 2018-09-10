d7dc15ddd80b3d58ef9edb3bf8151aa838b0bfda
lufei <905729088@qq.com>
2018-09-10 10:11:46 +0800

    9.5修改ip方式

--- src/components/ACommon/Api.js
+++ src/components/ACommon/Api.js
@@ -1,8 +1,6 @@
 import { initLAPI } from "@leither/l-api";
 export const G = {api:null,ip:null};
-//G.api = initLAPI('http://vzhan.cn'+'/webapi/')
-//G.api = initLAPI(location.origin+'/webapi/')//阿里云
-//G.ip = location.host;
+//G.ip = location.host;//阿里云
 G.ip = '192.168.1.187'
 G.api = initLAPI(`http://${G.ip}/webapi/`);
 
